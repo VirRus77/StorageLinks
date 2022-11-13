52686bb5321f388bf723207b15bb5d6804eab463
VirRus77 <virrus77@gmail.com>
2022-11-13 21:45:42 +0300

    Timer class Object.

--- LuaScripts/Models/Timer.lua
+++ LuaScripts/Models/Timer.lua
@@ -9,63 +9,51 @@ Author: Sotin NU aka VirRus77
 ---@field Delay Timer # Period delay of callback
 ---@field Callback fun() # Callback function
 Timer = { }
+---@type Object|Timer
+Timer = Object:extend(Timer)
 
---- func desc
----@param periodDelta number
-function Timer:AppendDelta (periodDelta)
-end
-
---- func desc
-function Timer:Immediately ()
-end
-
---- Constructor
+-- Constructor
 ---@param period number # In seconds.
 ---@param callback fun() # Callback function.
----@param delay? number # In seconds.
+---@param delay? number # Start delay In seconds.
 ---@return Timer # New instance of Timer
-function Timer.new (period, callback, delay)
+function Timer.new(period, callback, delay)
     ---@type Timer
-    local newInstance = { }
-
-    newInstance._workDelay = false
-    newInstance._ticker = 0.0
-    newInstance.Period = period
-    newInstance.Callback = callback
-
-    if(delay ~= nil and delay > 0.0) then
-        newInstance.Delay = Timer.new(delay, callback)
-    end
-
-    --- func desc
-    ---@param periodDelta number
-    ---@return boolean # Callback invoke
-    function newInstance:AppendDelta (periodDelta)
-        if (self._workDelay) then
-            self._workDelay = not self.Delay:AppendDelta (periodDelta)
-        end
-        self._ticker = self._ticker + periodDelta
-        if (self._ticker > self.Period) then
-            self._ticker = 0
-            if (self.Delay ~= nil) then
-                self._workDelay = true
-            else
-                self.Callback()
-            end
-            return true
-        end
-        return false
-    end
-
-    function newInstance:Immediately()
-        self.Callback()
-    end
-
-    setmetatable(newInstance, Timer)
-    newInstance.__index = Timer
+    local newInstance = Timer:make(period, callback, delay)
     return newInstance
 end
 
+---@param period number # In seconds.
+---@param callback fun() # Callback function.
+---@param delay? number # Start delay In seconds.
+function Timer:initialize(period, callback, delay)
+    self._workDelay = false
+    self._ticker = delay or 0.0
+    self.Period = period
+    self.Callback = callback
+end
+
+--- func desc
+---@param periodDelta number
+---@return boolean # Callback invoke
+function Timer:AppendDelta (periodDelta)
+    self._ticker = self._ticker + periodDelta
+    if (self._ticker > self.Period) then
+        self._ticker = 0
+        self.Callback()
+        return true
+    end
+    return false
+end
+
+--- Call callback immediately.
+function Timer:Immediately ()
+    self.Callback()
+end
+
+--- Conver millisecondsToSeconds.
+---@param milliseconds number
+---@return number
 function Timer.MillisecondsToSeconds(milliseconds)
     if (milliseconds == 0) then
         error ("Timer.MilisecondsToSeconds. milliseconds eq 0.", 100)
